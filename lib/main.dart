9d297b3ac4460a4975cad8cee03ae36330ac2426
MehediAsif2 <120641634+MehediAsif2@users.noreply.github.com>
2024-11-07 07:46:18 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( const MyFirstApp());
+void main() {
+  runApp(const MyFirstApp());
 }
+
 class MyFirstApp extends StatelessWidget {
   const MyFirstApp({super.key});
 
@@ -11,13 +12,23 @@ class MyFirstApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 200.0,
-            width: double.infinity,
-            color: Colors.blue,
-              child: const Text('Hello',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-
+        body: Container(
+          color: Colors.grey,
+          child: const Center(
+            child: Padding(
+              padding: EdgeInsets.all(12.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 50.0,
+                child: Icon(Icons.ac_unit_outlined),
+              ),
+              // child: Card(
+              //   elevation:12.0 ,
+              //     shadowColor: Colors.purple,
+              //
+              //     child: Image.network(
+              //         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJn2C3M5bAK5JbzqGGZpNxQ6J1wIuExZTHiA&s')),
+            ),
           ),
         ),
       ),
