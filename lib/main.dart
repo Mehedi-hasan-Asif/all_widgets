42423d40140d34aa7f5cc2331264d2a1b7d218b0
MehediAsif2 <120641634+MehediAsif2@users.noreply.github.com>
2024-11-11 08:21:42 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( const MyFirstApp());
+void main() {
+  runApp(const MyFirstApp());
 }
+
 class MyFirstApp extends StatelessWidget {
   const MyFirstApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 200.0,
-            width: double.infinity,
-            color: Colors.blue,
-              child: const Text('Hello',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+      home: Home(),
+    );
+  }
+}
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //past
+    // print(MediaQuery.of(context).size);
+    // print(MediaQuery.of(context).size.width);
+    // print(MediaQuery.of(context).size.height);
+    // print(MediaQuery.of(context).orientation);
+    // print(MediaQuery.of(context).devicePixelRatio);
+    // print(MediaQuery.of(context).displayFeatures);
+    // //present
+    // print(MediaQuery.sizeOf(context));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Media Query'),
+      ),
+      body: Center(
+        child: Container(
+          height: MediaQuery.sizeOf(context).height / 3,
+          width: MediaQuery.sizeOf(context).width - 100,
+          color: Colors.green,
+          alignment: Alignment.center,
+          child: const Text(
+            'Hello',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
         ),
       ),
